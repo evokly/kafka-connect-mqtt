b00d3974d0e9641bab548d7012eee83d09748c11
Tomasz Pietrzak <tomasz.pietrzak@pietrum.pl>
2016-02-10 18:25:10 +0100

    ref(constant): cleanup file

--- src/main/java/com/evokly/kafka/connect/mqtt/MqttSourceConstant.java
+++ src/main/java/com/evokly/kafka/connect/mqtt/MqttSourceConstant.java
@@ -1,18 +1,17 @@
 /**
  * Copyright 2016 Evokly S.A.
- *
- * <p>See LICENSE file for License</p>
+ * See LICENSE file for License
  **/
 
 package com.evokly.kafka.connect.mqtt;
 
 public class MqttSourceConstant {
-    public static final String CONNECTIONS              = "connections";
-    public static final String PREFIX                   = "connection[{}].";
+    public static final String CONNECTIONS = "connections";
+    public static final String PREFIX = "connection[{}].";
 
-    public static final String KAFKA_TOPIC              = "kafka.topic";
+    public static final String KAFKA_TOPIC = "kafka.topic";
 
-    public static final String MQTT_BROKER_URLS         = "mqtt.broker_urls";
-    public static final String MQTT_TOPIC               = "mqtt.topic";
-    public static final String MQTT_QUALITY_OF_SERVICE  = "mqtt.qos";
+    public static final String MQTT_BROKER_URLS = "mqtt.broker_urls";
+    public static final String MQTT_TOPIC = "mqtt.topic";
+    public static final String MQTT_QUALITY_OF_SERVICE = "mqtt.qos";
 }
