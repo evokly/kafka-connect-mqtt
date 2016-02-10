3a90d6b8b12744a7b580c0cc2a844dffe6604629
Tomasz Pietrzak <tomasz.pietrzak@pietrum.pl>
2016-02-10 21:31:10 +0100

    ref(Task): update logging statements with JavaDoc comments

--- src/main/java/com/evokly/kafka/connect/mqtt/MqttSourceTask.java
+++ src/main/java/com/evokly/kafka/connect/mqtt/MqttSourceTask.java
@@ -1,19 +1,21 @@
 /**
  * Copyright 2016 Evokly S.A.
- * <p>See LICENSE file for License</p>
+ * See LICENSE file for License
  **/
 
 package com.evokly.kafka.connect.mqtt;
 
+import com.evokly.kafka.connect.mqtt.util.Version;
+
 import org.apache.kafka.connect.source.SourceRecord;
 import org.apache.kafka.connect.source.SourceTask;
 
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
-import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 import org.slf4j.Logger;
@@ -22,8 +24,6 @@ import org.slf4j.LoggerFactory;
 import java.util.List;
 import java.util.Map;
 
-import com.evokly.kafka.connect.mqtt.util.Version;
-
 /**
  * MqttSourceTask is a Kafka Connect SourceTask implementation that reads
  * from MQTT and generates Kafka Connect records.
@@ -31,14 +31,25 @@ import com.evokly.kafka.connect.mqtt.util.Version;
 public class MqttSourceTask extends SourceTask implements MqttCallback {
     private static final Logger log = LoggerFactory.getLogger(MqttSourceConnector.class);
 
-    MqttClient client;
-    String mqttClientId = MqttClient.generateClientId();
+    MqttClient mClient;
+    String mMqttClientId = MqttClient.generateClientId();
 
+    /**
+     * Get the version of this task. Usually this should be the same as the corresponding
+     * {@link MqttSourceConnector} class's version.
+     *
+     * @return the version, formatted as a String
+     */
     @Override
     public String version() {
         return Version.getVersion();
     }
 
+    /**
+     * Start the Task.
+     *
+     * @param props initial configuration
+     */
     @Override
     public void start(Map<String, String> props) {
         // Setup MQTT Client
@@ -46,17 +57,18 @@ public class MqttSourceTask extends SourceTask implements MqttCallback {
 
         connectOptions.setCleanSession(true);
         connectOptions.setKeepAliveInterval(30);
-//        connectOptions.setServerURIs(props.get(MqttSourceConstant.MQTT_BROKER_URLS).split(","));
+        // connectOptions.setServerURIs(props.get(MqttSourceConstant.MQTT_BROKER_URLS).split(","));
 
         // Connect to Broker
         try {
-            client = new MqttClient(props.get(MqttSourceConstant.MQTT_BROKER_URLS), mqttClientId, new MemoryPersistence());
-            client.setCallback(this);
-            client.connect(connectOptions);
+            mClient = new MqttClient(props.get(MqttSourceConstant.MQTT_BROKER_URLS), mMqttClientId,
+                    new MemoryPersistence());
+            mClient.setCallback(this);
+            mClient.connect(connectOptions);
 
-            log.info("[{}] Connected to Broker", mqttClientId);
+            log.info("[{}] Connected to Broker", mMqttClientId);
         } catch (MqttException e) {
-            log.error("[{}] Connection to Broker failed!", mqttClientId);
+            log.error("[{}] Connection to Broker failed!", mMqttClientId, e);
         }
 
         // Setup topic
@@ -64,47 +76,58 @@ public class MqttSourceTask extends SourceTask implements MqttCallback {
             String topic = props.get(MqttSourceConstant.MQTT_TOPIC);
             Integer qos = Integer.parseInt(props.get(MqttSourceConstant.MQTT_QUALITY_OF_SERVICE));
 
-            client.subscribe(topic, qos);
+            mClient.subscribe(topic, qos);
 
             log.info("MQTT subscribe to " + topic + " with QoS " + qos.toString());
         } catch (MqttException e) {
-            log.error("MQTT subscribe failed! " + e.getMessage());
+            log.error("MQTT subscribe failed! " + e);
         }
     }
 
+    /**
+     * Stop this task.
+     */
     @Override
     public void stop() {
         try {
-            client.disconnect();
+            mClient.disconnect();
 
             log.info("MQTT disconnected.");
         } catch (MqttException e) {
-            log.info("MQTT disconnecting failed! " + e.getMessage());
+            log.error("MQTT disconnecting failed!", e);
         }
     }
 
+    /**
+     * Poll this SourceTask for new records. This method should block if no data is currently
+     * available.
+     *
+     * @return a list of source records
+     *
+     * @throws InterruptedException thread is waiting, sleeping, or otherwise occupied,
+     *                              and the thread is interrupted, either before or during the
+     *                              activity
+     */
     @Override
     public List<SourceRecord> poll() throws InterruptedException {
         return null;
     }
 
     /**
-     * Connection Lost
-     * This callback is invoked upon losing the MQTT connection.
+     * This method is called when the connection to the server is lost.
      *
-     * @param cause
+     * @param cause the reason behind the loss of connection.
      */
     @Override
     public void connectionLost(Throwable cause) {
-        log.info("MQTT connection lost!");
+        log.info("MQTT connection lost!", cause);
     }
 
     /**
-     * Delivery Complete
-     * This callback is invoked when a message published by this client
-     * is successfully received by the broker.
+     * Called when delivery for a message has been completed, and all acknowledgments have been
+     * received.
      *
-     * @param token
+     * @param token the delivery token associated with the message.
      */
     @Override
     public void deliveryComplete(IMqttDeliveryToken token) {
@@ -112,12 +135,13 @@ public class MqttSourceTask extends SourceTask implements MqttCallback {
     }
 
     /**
-     * Message Arrived
-     * This callback is invoked when a message is received on a subscribed topic.
+     * This method is called when a message arrives from the server.
      *
-     * @param topic
-     * @param message
-     * @throws Exception
+     * @param topic   name of the topic on the message was published to
+     * @param message the actual message.
+     *
+     * @throws Exception if a terminal error has occurred, and the client should be
+     *                   shut down.
      */
     @Override
     public void messageArrived(String topic, MqttMessage message) throws Exception {
--- src/test/java/MqttSourceConnectorTest.java
+++ src/test/java/MqttSourceConnectorTest.java
@@ -1,4 +1,4 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
